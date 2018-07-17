d0304f57347b5a9a0bc21919baf61792dff47afd
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2018-07-18 00:43:42 +0200

    Add basic logging, fix C++ directory bug and implement code style

--- bin/dabs.py
+++ bin/dabs.py
@@ -25,6 +25,7 @@ Options:
 
 
 def build_files(n, args):
+    print("Setting up build files")
     exe_ext = ".exe" if os.name == "nt" else ""
     obj_ext = ".o"
 
@@ -40,17 +41,20 @@ def build_files(n, args):
         for filename in fnmatch.filter(filenames, "*.c"):
             src = os.path.join(root, filename)
             obj = os.path.join("obj", os.path.splitext(src)[0] + obj_ext)
+            print("Found file: {} ".format(src))
             n.build(obj, "cc", src)
             # n.build(obj, "cxx", src, precompiled_header)
             objects.append(obj)
 
     # C++
-    for filename in fnmatch.filter(filenames, "*.cpp"):
-        src = os.path.join(root, filename)
-        obj = os.path.join("obj", os.path.splitext(src)[0] + obj_ext)
-        n.build(obj, "cxx", src)
-        # n.build(obj, "cxx", src, precompiled_header)
-        objects.append(obj)
+    for root, dirnames, filenames in os.walk(args["-s"]):
+        for filename in fnmatch.filter(filenames, "*.cpp"):
+            src = os.path.join(root, filename)
+            obj = os.path.join("obj", os.path.splitext(src)[0] + obj_ext)
+            print("Found file: {} ".format(src))
+            n.build(obj, "cxx", src)
+            # n.build(obj, "cxx", src, precompiled_header)
+            objects.append(obj)
 
     if args["-l"]:
         path_name = "lib/lib" + args["-l"] + ".a"
@@ -62,6 +66,7 @@ def build_files(n, args):
 
 
 def build_rules(n, args, conf):
+    print("Setting up rules")
     n.variable("cc", conf.get("cc", "gcc"))
     n.variable("cxx", conf.get("cxx", "g++"))
 
@@ -76,9 +81,9 @@ def build_rules(n, args, conf):
     n.variable("optflags", "-O2" + native_flag + debug_flag)
 
     defines = [" -D" + d for d in args["-D"]]
-    includes = [" -I" + i for i in conf["includes"]]
-    search_directories = [" -L" + L for L in conf["search_directories"]]
-    libraries = [" -l" + l for l in conf["libraries"]]
+    includes = [" -I" + i for i in conf.get("includes", [])]
+    search_directories = [" -L" + L for L in conf.get("search_directories", [])]
+    libraries = [" -l" + l for l in conf.get("libraries", [])]
 
     libflags = ""
     libflags += "".join(defines)
@@ -114,58 +119,57 @@ def build_rules(n, args, conf):
 
 
 # Everything below this line is dependency boilerplate.
-
-
-try:
-    from docopt import docopt
-    import ninja_syntax
-except ImportError:
-    msg = """You are missing one or more dependencies, install using:
-    {} -m pip install --user docopt ninja-syntax pyyaml
+if __name__=="__main__":
+    try:
+        from docopt import docopt
+        import ninja_syntax
+    except ImportError:
+        msg = """You are missing one or more dependencies, install using:
+        {} -m pip install --user docopt ninja-syntax pyyaml
 If you are using an old version of Python, and don't have pip,
 download https://bootstrap.pypa.io/get-pip.py and run it."""
 
-    print(msg.format(os.path.basename(sys.executable)))
-    sys.exit(1)
+        print(msg.format(os.path.basename(sys.executable)))
+        sys.exit(1)
 
 
-args = docopt(__doc__)
+    args = docopt(__doc__)
 
-# Check if we're likely running for the first time.
-first_time = not os.path.isfile(args["-o"])
+    # Check if we're likely running for the first time.
+    first_time = not os.path.isfile(args["-o"])
 
-# Write ninja build file.
-with open(args["-o"], "w") as ninja_file, open(args["-c"], "r") as conf_file:
-    conf = yaml.load(conf_file)
-    n = ninja_syntax.Writer(ninja_file)
-    build_rules(n, args, conf)
-    build_files(n, args)
+    # Write ninja build file.
+    with open(args["-o"], "w") as ninja_file, open(args["-c"], "r") as conf_file:
+        conf = yaml.load(conf_file)
+        n = ninja_syntax.Writer(ninja_file)
+        build_rules(n, args, conf)
+        build_files(n, args)
 
-# Check if we have ninja in path.
-path = os.environ.get("PATH", os.defpath).split(os.pathsep)
-if sys.platform == "win32":
-    path.insert(0, os.curdir)
-    pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
-    files = ["ninja" + ext for ext in pathext]
-else:
-    files = ["ninja"]
+    # Check if we have ninja in path.
+    path = os.environ.get("PATH", os.defpath).split(os.pathsep)
+    if sys.platform == "win32":
+        path.insert(0, os.curdir)
+        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+        files = ["ninja" + ext for ext in pathext]
+    else:
+        files = ["ninja"]
 
 
-def is_exe(path):
-    return os.path.exists(path) and os.access(
-        path, os.F_OK | os.X_OK) and not os.path.isdir(path)
+    def is_exe(path):
+        return os.path.exists(path) and os.access(
+            path, os.F_OK | os.X_OK) and not os.path.isdir(path)
 
-has_ninja = any(is_exe(os.path.join(d, f)) for d in path for f in files)
+    has_ninja = any(is_exe(os.path.join(d, f)) for d in path for f in files)
 
-if first_time and not has_ninja:
-    msg = """It appears you're running configure.py for the first time, but do not have
-ninja in your path. On Windows we recommend simply downloading the binary:
-    https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip
-Extract anywhere in your path, or even inside this directory.
-On linux it's easiest to compile from source:
-    wget https://github.com/ninja-build/ninja/archive/v1.6.0.tar.gz
-    tar xf v1.6.0.tar.gz && rm v1.6.0.tar.gz && cd ninja-1.6.0
-    {} configure.py --bootstrap
-    sudo cp ninja /usr/local/bin
-This should only take half a minute or so."""
-    print(msg.format(os.path.basename(sys.executable)))
+    if first_time and not has_ninja:
+        msg = """It appears you're running configure.py for the first time, but do not have
+    ninja in your path. On Windows we recommend simply downloading the binary:
+        https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip
+    Extract anywhere in your path, or even inside this directory.
+    On linux it's easiest to compile from source:
+        wget https://github.com/ninja-build/ninja/archive/v1.6.0.tar.gz
+        tar xf v1.6.0.tar.gz && rm v1.6.0.tar.gz && cd ninja-1.6.0
+        {} configure.py --bootstrap
+        sudo cp ninja /usr/local/bin
+    This should only take half a minute or so."""
+        print(msg.format(os.path.basename(sys.executable)))
